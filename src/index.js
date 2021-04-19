9998e63946453b84c3d3ee10997b50b2bd6e1cb9
Gnong <jinhongdev@gmail.com>
2021-04-19 18:21:41 +0900

    계정 인증 로직 (keystore validation)

--- src/index.js
+++ src/index.js
@@ -1,81 +1,82 @@
+import Caver from "caver-js";
 
+const config = {
+  rpcURL: "https://api.baobab.klaytn.net:8651",
+};
+
+const cav = new Caver(config.rpcURL);
 const App = {
-
-  start: async function () {
-
+  auth: {
+    accessType: 'keystore',
+    keystore: '',
+    password: ''
   },
 
-  handleImport: async function () {
+  start: async function () {},
 
+  handleImport: async function () {
+    const fileReader = new FileReader();
+    console.log(fileReader.readAsText(event.target.files[0]));
+    fileReader.onload = (event) => {
+      try {
+        if (!this.checkValidKeystore(event.target.result)) {
+          $('#message').text('유효하지 않은 keystore1 파일입니다.');
+          return;
+        }
+        this.auth.keystore = event.target.result;
+        $('#message').text('keystore 통과, 비밀번호를 입력하세요.');
+        document.querySelector('#input-password').focus();
+      } catch (error) {
+        $('#message').text('유효하지 않은 keystore2 파일입니다.');
+        return;
+      }
+    };
   },
 
   handlePassword: async function () {
-
+    this.auth.password = event.target.value;
   },
 
-  handleLogin: async function () {
+  handleLogin: async function () {},
 
-  },
+  handleLogout: async function () {},
 
-  handleLogout: async function () {
+  generateNumbers: async function () {},
 
-  },
+  submitAnswer: async function () {},
 
-  generateNumbers: async function () {
+  deposit: async function () {},
 
-  },
+  callOwner: async function () {},
 
-  submitAnswer: async function () {
+  callContractBalance: async function () {},
 
-  },
-
-  deposit: async function () {
-
-  },
-
-  callOwner: async function () {
-
-  },
-
-  callContractBalance: async function () {
-
-  },
-
-  getWallet: function () {
-
-  },
+  getWallet: function () {},
 
   checkValidKeystore: function (keystore) {
-
+    const parsedKeystore = JSON.parse(keystore);
+    const isValidKeystore =
+      parsedKeystore.version &&
+      parsedKeystore.id &&
+      parsedKeystore.address &&
+      parsedKeystore.keyring;
+    alert(isValidKeystore);
+    return isValidKeystore;
   },
 
-  integrateWallet: function (privateKey) {
+  integrateWallet: function (privateKey) {},
 
-  },
+  reset: function () {},
 
-  reset: function () {
+  changeUI: async function (walletInstance) {},
 
-  },
+  removeWallet: function () {},
 
-  changeUI: async function (walletInstance) {
+  showTimer: function () {},
 
-  },
+  showSpinner: function () {},
 
-  removeWallet: function () {
-
-  },
-
-  showTimer: function () {
-
-  },
-
-  showSpinner: function () {
-
-  },
-
-  receiveKlay: function () {
-
-  }
+  receiveKlay: function () {},
 };
 
 window.App = App;
@@ -91,16 +92,16 @@ var opts = {
   radius: 45, // The radius of the inner circle
   scale: 1, // Scales overall size of the spinner
   corners: 1, // Corner roundness (0..1)
-  color: '#5bc0de', // CSS color or array of colors
-  fadeColor: 'transparent', // CSS color or array of colors
+  color: "#5bc0de", // CSS color or array of colors
+  fadeColor: "transparent", // CSS color or array of colors
   speed: 1, // Rounds per second
   rotate: 0, // The rotation offset
-  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+  animation: "spinner-line-fade-quick", // The CSS animation name for the lines
   direction: 1, // 1: clockwise, -1: counterclockwise
   zIndex: 2e9, // The z-index (defaults to 2000000000)
-  className: 'spinner', // The CSS class to assign to the spinner
-  top: '50%', // Top position relative to parent
-  left: '50%', // Left position relative to parent
-  shadow: '0 0 1px transparent', // Box-shadow for the lines
-  position: 'absolute' // Element positioning
+  className: "spinner", // The CSS class to assign to the spinner
+  top: "50%", // Top position relative to parent
+  left: "50%", // Left position relative to parent
+  shadow: "0 0 1px transparent", // Box-shadow for the lines
+  position: "absolute", // Element positioning
 };
